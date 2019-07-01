2d67dded012ce24aa87a6220971c66912af17f20
noareyenoar <noarlplp@gmail.com>
2019-07-01 20:24:53 +0700

    Done Instrall recyclerview without testing

--- app/src/main/java/com/porducer/recycle_view_test_project/MainActivity.java
+++ app/src/main/java/com/porducer/recycle_view_test_project/MainActivity.java
@@ -2,12 +2,34 @@ package com.porducer.recycle_view_test_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int data_number = 25;
+    protected String[] data_set;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        create_dataset();
+
+        recyclerView = (RecyclerView) findViewById(R.id.cn_recycleview);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new cn_adapter(data_set);
+        recyclerView.setAdapter(mAdapter);
+    }
+    public void create_dataset(){
+        data_set = new String[data_number];
+        for(int i=0 ; i<=data_number ; i=i+1) {
+            data_set[i] = "Example Row =" + i;
+        }
     }
 }
--- app/src/main/java/com/porducer/recycle_view_test_project/cn_adapter.java
+++ app/src/main/java/com/porducer/recycle_view_test_project/cn_adapter.java
@@ -1,4 +1,49 @@
 package com.porducer.recycle_view_test_project;
 
-public class cn_adapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>  {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class cn_adapter extends RecyclerView.Adapter<cn_adapter.ViewHolder>  {
+    private String[] this_data_set;
+    public cn_adapter(String[] data_set) {
+        this_data_set = data_set;
+    }
+
+
+    @NonNull
+    @Override   //Method to create and tell where is the recycler view to bind
+    public cn_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.cn_surf_layout, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override   //Method to set information into binded view
+    public void onBindViewHolder(@NonNull cn_adapter.ViewHolder viewHolder, int i) {
+        viewHolder.getTextView1().setText(this_data_set[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this_data_set.length;
+    }
+    // Class for tell which view in view holder to bind
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView1;
+
+
+        public ViewHolder(View v) {
+            super(v);
+            textView1 = (TextView) v.findViewById(R.id.textView_of_recyclerView);
+        }
+
+        public TextView getTextView1() {
+            return textView1;
+        }
+    }
 }
